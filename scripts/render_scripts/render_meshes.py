37bb324a5688a265baa67aa3d258600087b2bc16
Mohamed Sayed <mohameds@nianticlabs.com>
2024-02-14 17:07:30 +0000

    Ray caster and rendering script revamp

--- scripts/render_scripts/render_meshes.py
+++ scripts/render_scripts/render_meshes.py
@@ -1,19 +1,12 @@
-from collections import OrderedDict
-import pickle
-from pytorch3d.renderer import TexturesVertex
-from pytorch3d.structures import Meshes
 import torch
 
 from pathlib import Path
 
-import trimesh
 from geometryhints.datasets.scannet_dataset import ScannetDataset
 from geometryhints.options import OptionsHandler
-from geometryhints.tools.fusers_helper import OurFuser
-from geometryhints.tools.tsdf import TSDF
+from geometryhints.tools.partial_fuser import PartialFuser
 from geometryhints.utils.dataset_utils import get_dataset
-from geometryhints.utils.generic_utils import readlines
-import os
+from geometryhints.utils.generic_utils import readlines, to_gpu
 
 import numpy as np
 import open3d as o3d
@@ -108,88 +101,6 @@ class SimpleScanNetDataset(torch.utils.data.Dataset):
         return item_dict
 
 
-class PartialFuser:
-    def __init__(
-        self,
-        gt_mesh_path,
-        cached_depth_path: Path,
-    ) -> None:
-        """Fuses depths for a scan at a time."""
-        self.fuser = OurFuser(gt_path=gt_mesh_path, fusion_resolution=0.04, max_fusion_depth=4.0)
-        self.cached_depth_path = cached_depth_path
-
-        self.cached_depths = OrderedDict()
-        # get pickle files from the cached_depth_path
-        pickle_files = os.listdir(cached_depth_path)
-        # sort
-        pickle_files.sort()
-
-        for file in os.listdir(cached_depth_path):
-            if file.endswith(".pickle"):
-                # load the pickle file
-                with open(cached_depth_path / file, "rb") as f:
-                    self.cached_depths[int(file.split(".")[0])] = pickle.load(f)
-
-        self.next_frame_ind_to_fuse = 0
-        self.mesh = None
-
-        self.frame_ids = list(self.cached_depths.keys())
-        self.frame_ids.sort()
-
-    def get_mesh(self, query_frame_id: int):
-        """Returns partial mesh for the current frame_id. Will fuse frames up
-        to that point."""
-
-        updated_mesh = False
-
-        if self.next_frame_ind_to_fuse >= len(self.cached_depths):
-            return self.mesh
-
-        frame_id_to_fuse = self.frame_ids[self.next_frame_ind_to_fuse]
-        # check if we need to fuse.
-        if query_frame_id > frame_id_to_fuse:
-            # fuse
-            while frame_id_to_fuse < query_frame_id:
-                # pick the right depth
-                cached_data = self.cached_depths[frame_id_to_fuse]
-                # fuse
-                self.fuser.fuse_frames(
-                    depths_b1hw=cached_data["depth_pred_s0_b1hw"],
-                    K_b44=cached_data["K_s0_b44"],
-                    cam_T_world_b44=cached_data["cam_T_world_b44"],
-                    color_b3hw=None,
-                )
-                updated_mesh = True
-
-                self.next_frame_ind_to_fuse += 1
-                if self.next_frame_ind_to_fuse >= len(self.cached_depths):
-                    break
-                else:
-                    frame_id_to_fuse = self.frame_ids[self.next_frame_ind_to_fuse]
-
-        # if the mesh got updated, run marching cubes
-        if updated_mesh:
-            self.mesh, _, _ = self.fuser.get_mesh_pytorch3d(scale_to_world=True)
-
-        return self.mesh
-
-    def fuse_all_frames(self):
-        """Returns partial mesh for the current frame_id. Will fuse frames up
-        to that point."""
-
-        for frame_id_to_fuse in self.frame_ids:
-            # pick the right depth
-            cached_data = self.cached_depths[frame_id_to_fuse]
-            # fuse
-            self.fuser.fuse_frames(
-                depths_b1hw=cached_data["depth_pred_s0_b1hw"],
-                K_b44=cached_data["K_s0_b44"],
-                cam_T_world_b44=cached_data["cam_T_world_b44"],
-                color_b3hw=None,
-            )
-
-        return self.mesh
-
 
 def render_scene_meshes(
     scan_id: str,
@@ -199,6 +110,7 @@ def render_scene_meshes(
     cached_depth_path: Path,
     data_to_render: str,
     batch_size: int = 4,
+    depth_noise: float = 0.0,
 ):
     """Renders a scene's mesh with a depth renderer.
 
@@ -231,11 +143,132 @@ def render_scene_meshes(
     mesh_renderer = PyTorch3DMeshDepthRenderer(height=height, width=width)
 
     get_mesh_path = ScannetDataset.get_gt_mesh_path(opts.dataset_path, opts.split, scan_id)
-    partial_mesher = PartialFuser(gt_mesh_path=get_mesh_path, cached_depth_path=cached_depth_path)
+    partial_mesher = PartialFuser(gt_mesh_path=get_mesh_path, cached_depth_path=cached_depth_path, depth_noise=depth_noise)
+
+    image_list = []
+    with torch.no_grad():
+        mesh = partial_mesher.fuse_all_frames()
+        for batch in tqdm.tqdm(dataloader):
+            batch = to_gpu(batch, key_ignores=["frame_id_str"])
+            for elem_ind, depth_1hw in enumerate(batch["frame_id_str"]):
+
+                depth_1hw = mesh_renderer.render(
+                    mesh,
+                    batch["cam_T_world_b44"][elem_ind][None],
+                    batch["K_b44"][elem_ind][None],
+                )[0]
+
+                # save the depth map
+                depth_path = (
+                    render_output_path / f"rendered_depth_{batch['frame_id_str'][elem_ind]}.png"
+                )
+                depth_1hw[depth_1hw == -1] = 0
+                mask_1hw = depth_1hw != 0
+
+                numpy_depth = (depth_1hw.cpu().numpy().squeeze() * 256).astype("uint16")
+                Image.fromarray(numpy_depth).save(depth_path)
+
+                sampled_weights_1hw = None
+                if data_to_render == "both":
+                    K_b44 = batch["K_b44"][elem_ind][None].cuda()
+                    K_b44[:, 0] *= width
+                    K_b44[:, 1] *= height
+                    invK_b44 = torch.linalg.inv(K_b44)
+                    cam_points_b4N = backprojector(depth_1hw[None], invK_b44)
+                    world_points_b4N = (
+                        batch["world_T_cam_b44"][elem_ind][None].cuda() @ cam_points_b4N.cuda()
+                    )
+                    sampled_weights_N = partial_mesher.fuser.sample_tsdf(
+                        world_points_b4N[:, :3, :].squeeze(0).transpose(0, 1),
+                        what_to_sample="weights",
+                    )
+                    sampled_weights_1hw = sampled_weights_N.view(1, 192, 256)
+                    sampled_weights_1hw[~mask_1hw] = 0.0
+
+                    weights_path = (
+                        render_output_path
+                        / f"sampled_weights_{batch['frame_id_str'][elem_ind]}.png"
+                    )
+
+                    numpy_weights = (sampled_weights_1hw.cpu().numpy().squeeze() * 256).astype(
+                        "uint16"
+                    )
+                    Image.fromarray(numpy_weights).save(weights_path)
+
+                # for debug. make and dump a video.
+                colormapped_depth = colormap_image(depth_1hw, mask_1hw.float(), vmin=0.0, vmax=4)
+                if sampled_weights_1hw is not None:
+                    colormapped_weights = colormap_image(
+                        sampled_weights_1hw,
+                        vmin=0,
+                        vmax=1,
+                        colormap="magma",
+                        flip="False",
+                    )
+                    colormapped_depth = torch.cat(
+                        [colormapped_depth.cpu(), colormapped_weights.cpu()], dim=2
+                    )
+
+                numpy_depth = np.uint8(
+                    colormapped_depth.permute(1, 2, 0).cpu().detach().numpy() * 255
+                )
+                image_list.append(numpy_depth)
+
+    save_viz_video_frames(
+        image_list,
+        str(render_output_path / f"{scan_id}.mp4"),
+        fps=10,
+    )
+
+
+def render_scene_meshes_partial(
+    scan_id: str,
+    dataset_root: Path,
+    tuple_filepath: Path,
+    render_output_path: Path,
+    cached_depth_path: Path,
+    data_to_render: str,
+    batch_size: int = 4,
+    depth_noise: float = 0.0,
+):
+    """Renders a scene's mesh with a depth renderer. Partial fusion.
+
+    Args:
+        scan_id (str): The scan id.
+        dataset_root (Path): The path to the dataset folder with scans data.
+        tuple_filepath (Path): The path to the tuple file.
+        render_output_path (Path): The path to save the rendered depth maps.
+        mesh_path (Path): The path to the mesh ply file.
+
+    """
+    render_output_path.mkdir(exist_ok=True, parents=True)
+
+    dataset = SimpleScanNetDataset(
+        scan_name=scan_id,
+        scan_data_root=dataset_root,
+        tuple_filepath=tuple_filepath,
+    )
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, drop_last=False, shuffle=False
+    )
+
+    height = 192
+    width = 256
+
+    if data_to_render == "both":
+        backprojector = BackprojectDepth(height=height, width=width).cuda()
+
+    mesh_renderer = PyTorch3DMeshDepthRenderer(height=height, width=width)
+
+    get_mesh_path = ScannetDataset.get_gt_mesh_path(opts.dataset_path, opts.split, scan_id)
+    partial_mesher = PartialFuser(gt_mesh_path=get_mesh_path, cached_depth_path=cached_depth_path, depth_noise=depth_noise)
 
     image_list = []
     with torch.no_grad():
         for batch in tqdm.tqdm(dataloader):
+            batch = to_gpu(batch, key_ignores=["frame_id_str"])
+            
             for elem_ind, depth_1hw in enumerate(batch["frame_id_str"]):
                 mesh = partial_mesher.get_mesh(query_frame_id=int(batch["frame_id_str"][elem_ind]))
                 if mesh is not None:
@@ -309,7 +342,6 @@ def render_scene_meshes(
         fps=10,
     )
 
-
 def render_scenes(
     dataset_root: Path,
     scan_list: list[str],
@@ -318,11 +350,13 @@ def render_scenes(
     cached_depth_path: Path,
     batch_size: int,
     data_to_render: str,
+    partial: bool = False,
+    depth_noise: float = 0.0,
 ):
+    render_func = render_scene_meshes if not partial else render_scene_meshes_partial
     for scan_id in tqdm.tqdm(scan_list):
-        mesh_path = cached_depth_path / f"{scan_id}.ply"
         render_output_path = output_root / scan_id
-        render_scene_meshes(
+        render_func(
             scan_id=scan_id,
             dataset_root=dataset_root,
             tuple_filepath=tuple_filepath,
@@ -330,7 +364,9 @@ def render_scenes(
             cached_depth_path=cached_depth_path / scan_id,
             batch_size=batch_size,
             data_to_render=data_to_render,
+            depth_noise=depth_noise,
         )
+
 
 
 if __name__ == "__main__":
@@ -355,7 +391,14 @@ if __name__ == "__main__":
         required=True,
         help="Data to render and save. Can be 'depth' or 'both' for mesh depth renders and sampled TSDF weights.",
     )
-
+    option_handler.parser.add_argument(
+        "--depth_noise",
+        type=float,
+        required=True,
+        help="Noise to add to depth maps before fusing.",
+        default=0.0
+    )
+    
     option_handler.parse_and_merge_options(ignore_cl_args=False)
     option_handler.pretty_print_options()
     opts = option_handler.options
@@ -378,4 +421,5 @@ if __name__ == "__main__":
         cached_depth_path=opts.cached_depth_path,
         batch_size=opts.batch_size,
         data_to_render=opts.data_to_render,
+        depth_noise=opts.depth_noise,
     )
