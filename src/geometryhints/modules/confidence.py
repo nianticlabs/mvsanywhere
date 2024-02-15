c7c3bc444175ad91c3edf6094d57ac04678612a6
Filippo Aleotti <filippoaleotti@nianticlabs.com>
2024-02-15 10:09:36 +0000

    add cost volume confidence

--- src/geometryhints/modules/confidence.py
+++ src/geometryhints/modules/confidence.py
@@ -6,77 +6,21 @@ class ConfidenceMetric(torch.nn.Module):
         super().__init__()
 
 
-class MSM(ConfidenceMetric):
-    """Matching Score confidence measure"""
+class VolumeEntropyConfidence(ConfidenceMetric):
+    """Variance along each epipolar line"""
 
-    def __init__(self, is_cost: bool, use_sigmoid: bool = False):
-        """
-        Params:
-            is_cost: if true, the volume is a cost volume. Otherwise, it is a similarity volume
-            use_sigmoid: if true, values are squashed between [0,1]
-        """
+    def __init__(self):
         super().__init__()
-        self.is_cost = is_cost
-        self.use_sigmoid = use_sigmoid
+        self.softmax = torch.nn.Softmax(dim=1)
 
-    def forward(self, volume: torch.Tensor):
-        """Compute Matching Score confidence.
-        It gives the minimum (in case of a cost volume) or max (in case of similarity volume)
-        score for each depth plane.
-
+    def forward(self, volume: torch.Tensor) -> torch.Tensor:
+        """Compute the entropy along each ray.
         Params:
-            volume: tensor (B,D,H,W) representing a volume of costs or similarity scores
+            volume: tensor (B,D,H,W), where D is the number of depth bins
         Returns:
-            a confidence map (B,1,H,W). The higher the value the better the confidence.
+            a tensor (B,1,H,W) representing the entropy along each ray. The higher the entropy,
+            the lower the confidence.
         """
-        if self.is_cost:
-            conf, _ = torch.min(volume, dim=1, keepdim=True)
-            conf = -1 * conf
-        else:
-            conf, _ = torch.max(volume, dim=1, keepdim=True)
-
-        if self.use_sigmoid:
-            conf = torch.sigmoid(conf)
-        return conf
-
-
-class MMN(ConfidenceMetric):
-    """Maximum Margin Naive confidence measure"""
-
-    def __init__(self, is_cost: bool, use_sigmoid: bool = False):
-        """
-        Params:
-            is_cost: if true, the volume is a cost volume. Otherwise, it is a similarity volume
-            use_sigmoid: if true, values are squashed between [0,1]
-        """
-        super().__init__()
-        self.is_cost = is_cost
-        self.use_sigmoid = use_sigmoid
-
-    def forward(self, volume: torch.Tensor):
-        """Maximum Margin Naive confidence.
-        It computes the difference between the top2 min (max) and the top1 min(max) of the
-        cost (similarity) for each depth plane.
-
-        Params:
-            volume: tensor (B,D,H,W) representing a volume of costs or similarity scores
-        Returns:
-            a confidence map (B,1,H,W).
-        """
-        if self.is_cost:
-            # the difference between top2 and top1 is positive (top2>=top1), and the
-            # larger the difference the better top1 wrt top2 => high confidence!
-            tops, _ = torch.topk(volume, dim=1, k=2, sorted=True, largest=False)
-            top1 = tops[:, 0:1]
-            top2 = tops[:, 1:2]
-            conf = top2 - top1
-        else:
-            # the difference between top1 and top2 is positive (top1>=top2), and the
-            # larger the difference the better top1 wrt top2 => high confidence!
-            tops, _ = torch.topk(volume, dim=1, k=2, sorted=True, largest=True)
-            top1 = tops[:, 0:1]
-            top2 = tops[:, 1:2]
-            conf = top1 - top2
-        if self.use_sigmoid:
-            conf = torch.sigmoid(conf)
+        prob = self.softmax(volume)
+        conf = -torch.sum(prob * torch.log(prob + 1e-5), dim=1, keepdim=True)
         return conf
--- tests/unit/confidence/test_cv_confidence.py
+++ tests/unit/confidence/test_cv_confidence.py
@@ -1,56 +1,34 @@
-import pytest
 import torch
 
-from src.geometryhints.modules.confidence import MM, MSM
+from src.geometryhints.modules.confidence import VolumeEntropyConfidence
 
 
-def build_fake_volume() -> torch.Tensor:
+def build_fake_volume_high_entropy() -> torch.Tensor:
     """Creates a fake cost volume with values from 1 and 64"""
     scores = torch.linspace(1, 64, 64).reshape(1, 64, 1, 1).repeat(1, 1, 12, 12)
     return scores
 
 
-@pytest.mark.parametrize("use_sigmoid", (True, False))
-def test_MM_cost(use_sigmoid: bool):
-    volume = build_fake_volume()
-    mm_metric = MM(is_cost=True, use_sigmoid=use_sigmoid)
-    confidence = mm_metric.forward(volume=volume)
+def build_fake_volume_low_entropy() -> torch.Tensor:
+    """Creates a fake cost volume with values from 1 and 64"""
+    scores = torch.ones((1, 64, 12, 12), dtype=torch.float32)
+    scores[:, 1, :, :] = 100.0
+    return scores
+
+
+def test_volume_entropy_confidence_high_entropy():
+    volume = build_fake_volume_high_entropy()
+    conf_metric = VolumeEntropyConfidence()
+    confidence = conf_metric.forward(volume=volume)
     assert confidence.shape == (1, 1, 12, 12)
-
-    expected = torch.sigmoid(torch.tensor(1)) if use_sigmoid else 1
-    assert confidence[0, 0, 0, 0] == expected
+    assert torch.isclose(confidence[0, 0, 0, 0], torch.tensor(1.0405), atol=1e-4)
 
 
-@pytest.mark.parametrize("use_sigmoid", (True, False))
-def test_MM_similarity(use_sigmoid: bool):
-    volume = build_fake_volume()
-    mm_metric = MM(is_cost=False, use_sigmoid=use_sigmoid)
-    confidence = mm_metric.forward(volume=volume)
+def test_volume_entropy_confidence_low_entropy():
+    volume = build_fake_volume_low_entropy()
+    conf_metric = VolumeEntropyConfidence()
+    confidence = conf_metric.forward(volume=volume)
     assert confidence.shape == (1, 1, 12, 12)
-
-    expected = torch.sigmoid(torch.tensor(1)) if use_sigmoid else 1
-    assert confidence[0, 0, 0, 0] == expected
-
-
-@pytest.mark.parametrize("use_sigmoid", (True, False))
-def test_MSM_cost(use_sigmoid: bool):
-    volume = build_fake_volume()
-    msm_metric = MSM(is_cost=True, use_sigmoid=use_sigmoid)
-
-    confidence = msm_metric.forward(volume=volume)
-    assert confidence.shape == (1, 1, 12, 12)
-
-    expected = torch.sigmoid(torch.tensor(-1)) if use_sigmoid else -1
-    assert confidence[0, 0, 0, 0] == expected
-
-
-@pytest.mark.parametrize("use_sigmoid", (True, False))
-def test_MSM_similarity(use_sigmoid: bool):
-    volume = build_fake_volume()
-    msm_metric = MSM(is_cost=False, use_sigmoid=use_sigmoid)
-
-    confidence = msm_metric.forward(volume=volume)
-    assert confidence.shape == (1, 1, 12, 12)
-
-    expected = torch.sigmoid(torch.tensor(64)) if use_sigmoid else 64
-    assert confidence[0, 0, 0, 0] == expected
+    assert torch.isclose(
+        confidence[0, 0, 0, 0], torch.tensor(-1.0014e-05, dtype=torch.float32), atol=1e-4
+    )
